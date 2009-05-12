6490ca87e79cacd6bfaedb8a5721bb2ae5c78585
Matthew Good <matthew@cytobank.org>
2009-05-12 16:57:57 -0700

    added capfile, config/deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :user, "mgood"
+
+
+
+set :application, "capistrano.test"
+
+puts "may need to fully qualify the URL here"
+set :deploy_to, "/Users/mgood/Sites/captest"
+
+
+##### Source Code Management ######3
+puts "scm_username *may* cause an issue, but doubtful.  If needed, just put username into some other variable name, and dynamically load into the repo info."
+set :scm, :git
+set :scm_username, "cytobank-matthew"
+set :repository,  "git@github-cytobank:#{scm_username}/moretests.git"
+
+set :use_sudo, 'false'
+
+set :user, "mgood"
+set :domain, "192.168.1.107"
+
+role :app, "192.168.1.107"
+role :web, "192.168.1.107"
+role :db, "192.168.1.107", :primary => true
+
+=begin
+puts "note that the db connection will likely not work = I believe its configured to only be accessible locally"
+server "#{domain}/~#{user}", :app, :web, :db, :primary => true
+
+#role :app, :domain
+#role :web, :domain
+#role :db,  localhost"your db-server here", :primary => true
+
+=end
