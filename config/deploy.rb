64a786c6c21dc3a073e4af552c96f9bdcf89faa4
Matthew Good <matthew@cytobank.org>
2009-05-12 18:48:49 -0700

    made some capistrano changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,18 @@
 set :user, "mgood"
 
-
-
 set :application, "capistrano.test"
 
-puts "may need to fully qualify the URL here"
 set :deploy_to, "/Users/mgood/Sites/captest"
 
-
 ##### Source Code Management ######3
-puts "scm_username *may* cause an issue, but doubtful.  If needed, just put username into some other variable name, and dynamically load into the repo info."
 set :scm, :git
-set :scm_username, "cytobank-matthew"
-set :repository,  "git@github-cytobank:#{scm_username}/moretests.git"
+set :scm_command, "/usr/local/git/bin/git"
 
+#set :scm_username, "cytobank-matthew"
+#set :repository,  "git@github-cytobank:#{scm_username}/moretests.git"
+set :repository, "git@github.com:cytobank-matthew/moretests.git"
+
+#set :copy_strategy, :export
 
 #### Deployment Server Info ####
 # setting :paranoid = false will prevent a server host fingerprint check.
@@ -23,6 +22,7 @@ set :use_sudo, 'false'
 
 set :user, "mgood"
 set :domain, "192.168.1.107"
+puts "passed the domain setter"
 
 role :app, "192.168.1.107"
 role :web, "192.168.1.107"
